7fdc61f23ec48358ef652f7b77d0d1282e48dffc
ineuron1979 <56010086+ineuron1979@users.noreply.github.com>
2019-10-13 21:03:38 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hey")
+import timeit
+code_to_test = """
+a = range(100000)
+b = []
+for i in a:
+    b.append(i*2)
+"""
+elapsed_time = timeit.timeit(code_to_test, number=100)/100
+print(elapsed_time)
